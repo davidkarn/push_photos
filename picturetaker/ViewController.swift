5c3ea6ab20ddc6df4fcaa6eefeeb32e4b838c339
David Karn <davidkarnseattle@gmail.com>
2017-03-18 18:20:33 -0700

    include heading, push to rest api

--- picturetaker/ViewController.swift
+++ picturetaker/ViewController.swift
@@ -19,10 +19,73 @@ class ViewController: UIViewController, CLLocationManagerDelegate,
     var session: AVCaptureSession?
     var stillImageOutput: AVCaptureStillImageOutput?
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var image : UIImage?
 
     @IBOutlet weak var preview: UIView!
     @IBOutlet weak var image_view: UIImageView!
     @IBOutlet weak var picture_button: UIButton!
+
+
+    func message_form_data(_ boundary:String,
+                           parameters: [String: String]?) -> Data {
+        var body = Data()
+
+        if parameters != nil {
+            for (key, value) in parameters! {
+                body.append(Data("--\(boundary)\r\n".utf8))
+                body.append(Data("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n".utf8))
+                body.append(Data("\(value)\r\n".utf8)) }}
+
+        body.append(Data("--\(boundary)\r\n".utf8))
+        body.append(Data("Content-Disposition: form-data; name=\"image\"; filename=\"image.jpeg\"\r\n".utf8))
+        body.append(Data("Content-Type: image/jpeg\r\n\r\n".utf8))
+        let image_data = UIImageJPEGRepresentation(self.image!, 0.85)
+        body.append(image_data!)
+        body.append(Data("\r\n".utf8))
+
+        body.append(Data("--\(boundary)--\r\n".utf8))
+
+        print("payload here:")
+        print(parameters)
+        print(String(describing: body))
+        print(body)
+
+        return body as Data}
+
+    func generate_boundary() -> String {
+        return "Boundary-\(UUID().uuidString)" }
+
+
+    func post_photo(
+        _ completion_handler: @escaping (URLResponse?, Data?, Error?) -> Void) {
+        print("pressed post message in")
+        let url        : String   = "https://google.com/post_image"
+        let request               = NSMutableURLRequest()
+
+        request.url = URL(string: url)
+        request.httpMethod = "PUT"
+
+        let location = get_location_object()
+
+        let boundary = generate_boundary()
+        let fullData = message_form_data(
+            boundary,
+            parameters: location)
+
+        request.setValue("multipart/form-data; boundary=" + boundary,
+                         forHTTPHeaderField: "Content-Type")
+        // REQUIRED!
+        request.setValue(String(fullData.count), forHTTPHeaderField: "Content-Length")
+
+        request.httpBody                = fullData
+        request.httpShouldHandleCookies = true
+
+        let queue:OperationQueue       = OperationQueue()
+
+        NSURLConnection.sendAsynchronousRequest(
+            request as URLRequest,
+            queue:               queue,
+            completionHandler:   completion_handler as! (URLResponse?, Data?, Error?) -> Void) }
 
     @IBAction func take_picture_tapped(_ sender: Any) {
         if let videoConnection = stillImageOutput!.connection(withMediaType: AVMediaTypeVideo) {
@@ -33,8 +96,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate,
             let dataProvider = CGDataProvider(data: imageData as! CFData)
             let cgImageRef = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: CGColorRenderingIntent.defaultIntent)
             let image = UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.right)
+            self.image = image
             // ...
             // Add the image to captureImageView here...
+            self.post_photo({(response: URLResponse?, data: Data?, error: Error?) -> Void in
+                OperationQueue.main.addOperation {
+                    print("got response", data)}})
 
             self.image_view.image = image }})}}
 
@@ -92,13 +159,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate,
 
     }
 
-    func get_location_object() -> [String: AnyObject] {
+    func get_location_object() -> [String: String] {
         if (locationManager.location == nil) {
             return [:] }
-        return ["altitude": locationManager.location!.altitude as AnyObject,
-                "latitude": locationManager.location!.coordinate.latitude as AnyObject,
-                "longitude": locationManager.location!.coordinate.longitude as AnyObject,
-                "floor": locationManager.location!.floor != nil ? locationManager.location!.floor!.level as AnyObject : false as AnyObject] }
+        var heading = ""
+        if (locationManager.heading! != nil) {
+            heading = String(locationManager.heading!.magneticHeading)
+        }
+        return ["altitude": String(locationManager.location!.altitude),
+                "course":   String(locationManager.location!.course),
+                "heading":   heading,
+                "latitude": String(locationManager.location!.coordinate.latitude),
+                "longitude": String(locationManager.location!.coordinate.longitude),
+                "floor": String(locationManager.location!.floor != nil ? String(locationManager.location!.floor!.level) : "")]}
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -109,6 +182,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate,
         locationManager.delegate = self;
         locationManager.distanceFilter = kCLLocationAccuracyNearestTenMeters;
         locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        if (CLLocationManager.headingAvailable()) {
+            locationManager.headingFilter = 1
+            locationManager.startUpdatingHeading()
+            locationManager.delegate = self
+        }
 
         load_camera() }
 
